d2e44caa7af02c0376c5c78dc64f4350def1baf9
Martastain <martas@imm.cz>
2018-10-17 14:31:53 +0200

    added services to default config

--- nebulacore/common.py
+++ nebulacore/common.py
@@ -42,6 +42,7 @@ class Config(dict):
         self["views"] = {}
         self["meta_types"] = {}
         self["actions"] = {}
+        self["services"] = {}
 
         if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
             local_settings_path = sys.argv[1]
